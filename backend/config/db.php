db1ec16d9407de92717950947c7eddc6937a8cee
Giovanni Fiori <giovannicfiori@gmail.com>
2018-10-17 22:16:39 -0400

    Deletado arquivo desnecessário

--- backend/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => 'secretpw',
-    'charset' => 'utf8',
-];
