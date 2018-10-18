f630f0a8f7cee031287d483c27ee763084cfaa98
Giovanni Fiori <giovannicfiori@gmail.com>
2018-10-18 17:18:00 -0400

    Tela de visualizar despesa após cadastro

--- backend/views/despesa/view.php
+++ backend/views/despesa/view.php
@@ -2,22 +2,24 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use backend\models\Despesa;
+use backend\models\Fornecedor;
+use backend\models\Beneficiario;
+use backend\models\Item;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Despesa */
 
-$this->title = $model->id;
+$this->title = "Despesa " . $despesaModel->id;
 $this->params['breadcrumbs'][] = ['label' => 'Despesas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="despesa-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Editar', ['update', 'id' => $despesaModel->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Deletar', ['delete', 'id' => $despesaModel->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
@@ -27,22 +29,69 @@ $this->params['breadcrumbs'][] = $this->title;
     </p>
 
     <?= DetailView::widget([
-        'model' => $model,
+        'model' => $despesaModel,
         'attributes' => [
             'id',
-            'valor_unitario',
+            [
+                'attribute' => 'valor_unitario',
+                'value' => function($model){
+                    return "R$" . $model->valor_unitario;
+                }
+            ],
             'qtde',
+            [
+                'label' => 'Valor total',
+                'value' => function($model){
+                    return "R$" . ($model->valor_unitario * $model->qtde);
+                }
+            ],
             'tipo_desp',
-            'status',
-            'data_emissao_NF',
+            [
+                'attribute' => 'status',
+                'value' => function($model){
+                    return $model->getStatus()[$model->status];
+                }
+            ],
+            [
+                'attribute' => 'data_emissao_NF',
+                'value' => function($model){
+                    return date('d/m/Y', strtotime($model->data_emissao_NF));
+                }
+            ],
             'pendencias:ntext',
             'numero_cheque',
-            'data_pgto',
+            [
+                'attribute' => 'data_pgto',
+                'value' => function($model){
+                    return date('d/m/Y', strtotime($model->data_pgto));
+                }
+            ],
             'nf_recibo',
             'objetivo:ntext',
-            'id_beneficiario',
-            'id_fornecedor',
-            'id_item',
+            [
+                'attribute' => 'id_beneficiario',
+                'label' => 'Beneficiário',
+                'value' => function($model) {
+                    $b = Beneficiario::findOne($model->id_beneficiario);
+                    return isset($b) ? $b->nome . " - " . $b->rg : "Beneficiário não registrado";
+                }
+            ],
+            [
+                'attribute' => 'id_fornecedor',
+                'label' => 'Fornecedor',
+                'value' => function($model) {
+                    $f = Fornecedor::findOne($model->id_fornecedor);
+                    return isset($f) ? $f->nome . " - " . $f->cpf_cnpj : "Fornecedor não registrado";
+                }
+            ],
+            [
+                'attribute' => 'id_item',
+                'label' => 'Item',
+                'value' => function($model) {
+                    $item = Item::findOne($model->id_item);
+                    return isset($item) ? $item->descricao : "Item não registrado";
+                }
+            ],
         ],
     ]) ?>
 
